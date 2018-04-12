5508a56daeef7c640658e1688c7f0706fba3fe40
Beno!t POLASZEK <bpolaszek@gmail.com>
2018-04-12 11:51:42 +0200

    Non-expiring lock + unlock option

--- src/Command/CpImportWatchCommand.php
+++ src/Command/CpImportWatchCommand.php
@@ -5,6 +5,7 @@ namespace BenTools\CpImportBundle\Command;
 use BenTools\CpImportBundle\CpImportFileWatcher;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Lock\Factory;
@@ -34,10 +35,25 @@ class CpImportWatchCommand extends Command
     /**
      * @inheritDoc
      */
+    protected function configure()
+    {
+        $this->addOption('unlock', null, InputOption::VALUE_NONE, 'Unlocks the command if lock.');
+    }
+
+    /**
+     * @inheritDoc
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output = new SymfonyStyle($input, $output);
-        $lock = $this->lockFactory->createLock(__FILE__);
+        $lock = $this->lockFactory->createLock(__FILE__, null);
+
+        if (true === $input->getOption('unlock')) {
+            $lock->release();
+            $output->success(sprintf('%s was successfully unlocked.', $this->getName()));
+            return;
+        }
+
         if ($lock->acquire()) {
             $this->watcher->wait();
             $lock->release();
